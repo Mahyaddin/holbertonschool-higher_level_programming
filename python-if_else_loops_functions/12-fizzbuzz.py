0dc9fbf4d42df0431ff45541d20ba51c714201eb
SəMahyaddin <13749@holbertonstudents.com>
2026-04-06 18:28:10 +0000

    Fix FizzBuzz: exact 415 bytes and PEP8 compliance

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def fizzbuzz():
+    """Prints numbers from 1 to 100 with FizzBuzz logic."""
     for i in range(1, 101):
         if i % 15 == 0:
             print("FizzBuzz", end="")
