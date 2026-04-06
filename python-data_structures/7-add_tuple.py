791aa75e3589149bc6c1de3a3cd98a0485ba39f5
SəMahyaddin <13749@holbertonstudents.com>
2026-04-06 20:02:53 +0000

    Fix Pycodestyle W293: remove whitespace in blank line

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    # Hər iki tuple-ın ilk iki elementini götürürük, yoxdursa 0 qoyuruq
     a = tuple_a + (0, 0)
     b = tuple_b + (0, 0)
-    
     return (a[0] + b[0], a[1] + b[1])
