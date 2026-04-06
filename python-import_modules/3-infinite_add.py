43cf05d87e2d96f3bfbdd84501304fa5d4c7d810
SəMahyaddin <13749@holbertonstudents.com>
2026-04-06 18:47:41 +0000

    Fix pycodestyle whitespace error in Task 3

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-
     total = 0
-    # sys.argv-nin 1-ci elementindən başlayaraq hamısını dövrə salırıq
     for i in range(1, len(sys.argv)):
         total += int(sys.argv[i])
-    
     print("{}".format(total))
