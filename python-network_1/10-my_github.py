cbf46304eb8645d2e9795924358e3f6ded4d8ebc
Mahyaddin <13749@holbertonstudents.com>
2026-04-15 19:23:28 +0000

    Fix: pycodestyle validation and complete network_1

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-""" Uses GitHub API to display your id """
+""" Uses GitHub API and Basic Auth to display user id """
 import requests
 import sys
 
 
 if __name__ == "__main__":
     username = sys.argv[1]
-    password = sys.argv[2]
-    r = requests.get('https://api.github.com/user', auth=(username, password))
+    token = sys.argv[2]
+    r = requests.get('https://api.github.com/user', auth=(username, token))
     try:
-        print(r.json().get('id'))
-    except:
+        json_res = r.json()
+        print(json_res.get('id'))
+    except Exception:
         print("None")
