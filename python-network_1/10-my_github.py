6beab8cc6debdb8c20a70b9863beeb791c1cbe9e
Mahyaddin <13749@holbertonstudents.com>
2026-04-15 19:11:31 +0000

    Complete Python Network #1 tasks 0-9

---
+++ python-network_1/10-my_github.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Uses GitHub API to display your id """
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    r = requests.get('https://api.github.com/user', auth=(username, password))
+    try:
+        print(r.json().get('id'))
+    except:
+        print("None")
